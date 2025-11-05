2cff082680d419dfb11b4ec294ee846641b85823
Chris Anzilotti <anzilottichr@gmail.com>
2025-11-05 02:55:12 -0500

    Player Controller
    Simple controller with mouse camera movement, and gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,68 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 5.0f;
+
+    [SerializeField]
+    private CharacterController controller;
+
+    private float xRotation = 0f;
+    public float mouseSensitivity = 100f;
+    public Transform playerCamera;
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+
+ 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleMovement();
+        handleCamera();
+        handleGravity();
+    }
+
+    private void handleCamera()
+    {
+        //Mouse Camera Controls
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    
+    private void handleGravity()
+    {
+        //gravity
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; //small negative value to keep player grounded
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    } 
+
+    private void handleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+         //Player Movement Controls
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
