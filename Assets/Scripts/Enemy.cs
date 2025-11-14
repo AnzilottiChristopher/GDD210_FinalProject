4411d7e43f924f7bda2785be331b56c5cb1d2ae4
Chris Anzilotti <anzilottichr@gmail.com>
2025-11-13 21:41:42 -0500

    Enemy AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class Enemy : MonoBehaviour
         Attacking, // Attacking the player
         Investigating, // Heard noise of player and looking for area
         Stunned, // Stunned by the player momentarily
-        Dead // Killed by player
+        Dead, // Killed by player
+        Patrolling, // Patrolling an area
     };
 
 
@@ -21,25 +23,32 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private int health = 3;
     [SerializeField] private float detectionRange;
+    [SerializeField] private float patrolRange;
     [SerializeField] private GameObject mainObj;
     [SerializeField] private GameObject eyes;
     [SerializeField] private States state;
+    private NavMeshAgent agent;
 
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mainObj = this.gameObject;
         eyes = mainObj.transform.GetChild(0).gameObject;
-        state = States.Idle;
+        state = States.Patrolling;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(state == States.Patrolling && (!agent.hasPath)) 
+        {
+            agent.SetDestination(GetPoint.Instance.GetRandomPoint(transform, patrolRange));
+        }
     }
     void FixedUpdate()
     {
@@ -48,7 +57,7 @@ public class Enemy : MonoBehaviour
             Debug.Log("Hit something");
         }
     }
-    
+
     private bool eyesight()
     {
         RaycastHit lineOfSight;
@@ -64,7 +73,13 @@ public class Enemy : MonoBehaviour
             Debug.DrawLine(eyes.transform.position, eyes.transform.position + eyes.transform.right
             * detectionRange, Color.green);
         }
-        
+
         return hit;
     }
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, patrolRange);
+    }
+#endif
 }
